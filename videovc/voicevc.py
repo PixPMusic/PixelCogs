1c2f15169bcb1fb1f4eb391b1dfe26c13ff13346
Pixel Perfect <pixelperfectpon3@gmail.com>
2020-04-11 23:06:21 -0500

    Python isn't weird about forwardslashes

--- videovc/voicevc.py
+++ videovc/voicevc.py
@@ -9,7 +9,7 @@ class VoiceVC(commands.Cog):
     async def vc(self, ctx):
         """Generate a video chat url"""
         try:
-            await ctx.send("Here's your link: https:////discordapp.com\/channels//" + str(ctx.guild.id) + "//" + str(ctx.author.voice.channel.id))
+            await ctx.send("Here's your link: https://discordapp.com/channels/" + str(ctx.guild.id) + "/" + str(ctx.author.voice.channel.id))
         except AttributeError:
             return await self._embed_msg(
                 ctx,
