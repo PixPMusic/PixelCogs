c7e768e97bc4b0b501a18af1833b0914c95b1d08
Pixel Perfect <pixelperfectpon3@gmail.com>
2020-04-14 02:10:31 -0500

    Try to implement failure message round 2

--- videovc/voicevc.py
+++ videovc/voicevc.py
@@ -11,7 +11,7 @@ class VoiceVC(commands.Cog):
         try:
             await ctx.send("Here's your link: https://discordapp.com/channels/" + str(ctx.guild.id) + "/" + str(ctx.author.voice.channel.id))
         except:
-            return await self._embed_msg(
+            await self._embed_msg(
                 ctx,
                 title=_("Unable To Generate Link"),
                 description=_("Connect to a voice channel first."),
