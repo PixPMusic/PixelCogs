8a456b5147fd95d73918f85b3ac70a394877490e
Pixel Perfect <pixelperfectpon3@gmail.com>
2020-04-14 02:08:44 -0500

    Try to implement failure message

--- videovc/voicevc.py
+++ videovc/voicevc.py
@@ -10,7 +10,7 @@ class VoiceVC(commands.Cog):
         """Generate a video chat url"""
         try:
             await ctx.send("Here's your link: https://discordapp.com/channels/" + str(ctx.guild.id) + "/" + str(ctx.author.voice.channel.id))
-        except AttributeError:
+        except:
             return await self._embed_msg(
                 ctx,
                 title=_("Unable To Generate Link"),
